d9ae8a61d62dc145e3c7141c43a95c13d151ad52
Rana Ahmed <raana.elwakeel@gmail.com>
2025-06-10 18:39:57 +0300

    Add files via upload

---
+++ Card component/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Card from './Crad'
+
+function App() {
+
+  return (
+    <>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ Card component/src/Crad.jsx
@@ -0,0 +1,14 @@
+function Card (){
+    return(
+        <div className="main-div">
+            <img src="../src/assets/Pokemon.jpg" alt="image"></img>
+            <h2>My name is Pokemon</h2>
+            <p>
+                This is a simple card of the famous character Pokemon
+            </p>
+        </div>
+        )
+}
+
+
+export default Card
